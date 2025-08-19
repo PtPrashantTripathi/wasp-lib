51c4f35fd04d4cc81ee54661277604caea726844
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2025-08-20 03:33:06 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        // Enable type checking during tests
-        typecheck: {
-            tsconfig: "./tsconfig.json",
-        },
-        coverage: {
-            reporter: ["text", "lcov"], // "lcov" zaruri hai GitHub upload ke liye
-            reportsDirectory: "./coverage",
-        },
+  test: {
+    typecheck: {
+      tsconfig: "./tsconfig.json",
     },
+    coverage: {
+      reporter: ["text", "lcov", "json-summary"], // add json-summary for PR report
+      reportsDirectory: "./coverage",
+    },
+  },
 });
